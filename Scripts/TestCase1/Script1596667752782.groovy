3097f4493e0cf9ed471acd7590f3dcdf979e8d5c
M Syauqy Yarlon <M Syauqy Yarlon@DESKTOP-0MDEI6V>
2020-08-06 05:50:17 +0700

    test1

--- Scripts/TestCase1/Script1596667752782.groovy
+++ Scripts/TestCase1/Script1596667752782.groovy
@@ -14,4 +14,9 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\M Syauqy Yarlon\\Downloads\\muslimpocket-41-v1.997.apk', false)
+
+Mobile.closeApplication()
 
